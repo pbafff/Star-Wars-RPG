fd3e0a062f4127777a559d357560de9ee5bf9a25
Andre Myers <andre.myers99@gmail.com>
2018-03-22 03:31:49 -0400

    testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-               
+    var x, y;
+    $(".character-select").on("click", ".character" ,function() {
+        x = $(this).offset().left;
+        y = $(this).offset().top;
+        alert(x);
+        alert(y);
         
+    });
+    $(".enemy-position").on("click", function() {
+        x = $(this).offset().left;
+        y = $(this).offset().top;
+        alert(x);
+        alert(y);
+    })
+    
         
         
         
