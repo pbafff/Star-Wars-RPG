c15493649de1863461fb1a6badb231cfbd8fc515
Andre Myers <andre.myers99@gmail.com>
2018-03-24 01:20:05 -0400

    added some move handling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var playerChar, enemyChar, playerAtk, enemyAtk, playerSpeed, enemySpeed;
+    var playerChar, enemyChar, playerAtk, enemyAtk, playerSpeed, enemySpeed, enemyMove;
     $(".character-select").on("click", ".character", function() {
         var thiS = $(this);
         playerChar = thiS.attr('id');
@@ -8,38 +8,63 @@ $(document).ready(function() {
         $("#slot1").append("<p>" + thiS.data('moves').move1 + "</p>");
         $("#slot2").append("<p>" + thiS.data('moves').move2 + "</p>");
         $("#slot1").click(function() {
-           console.log(playerDamageCalc($(this).text()));
+           isAtkOrSpecial($(this).text());
         });
         $("#slot2").click(function() {
-            console.log(playerDamageCalc($(this).text()));
+            isAtkOrSpecial($(this).text());
         });
     });
-//use setTimer to delay chosing of enemy, allow time for animation
-    $(".character-select").on("click", ".character", function() {
-        enemyChar = $(this).attr('id');
-        enemyAtk = $(this).data('attack');
-        enemySpeed = $(this).data('speed');
-    });
 
-   
+    $(".character-select").on("click", ".character", function() {
+        var thiS = $(this);
+        var m1, m2;
+        enemyChar = thiS.attr('id');
+        enemyAtk = thiS.data('attack');
+        enemySpeed = thiS.data('speed');
+        m1 = thiS.data('moves').move1;
+        m2 = thiS.data('moves').move2;
+
+    });
 
     var gameMoves = {
         saberStrike: {
             damage: 10,
             accuracy: 1,
+            type: "attack",
         },
         saberThrow: {
             damage: 11,
             accuracy: 0.7,
+            type: "attack",
         },
         forceLightning: {
             damage: 12,
             accuracy: 0.8,
+            type: "attack",
         },
-        forceFocus: 3,
+        forceFocus: {
+            amount: 3,
+            type: "speed",
+            target: "self",
+        },
+        forceConfusion: {
+            amount: 3,
+            type: "speed",
+            target: "opponent",
+        },
 
     };
     
+    function isAtkOrSpecial(move) {
+        if (gameMoves[move].type === "attack") {
+            playerDamageCalc(move);
+   
+
+        } else if (gameMoves[move].type === "speed") {
+            
+        }
+    }
+
     function playerDamageCalc(move) {
         return gameMoves[move].damage * playerAtk;
     }
